e79cdd300473cebad69569ef64948869548b2c57
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-01 13:00:17 +0200

    UPDATE show estimate arrive time

--- src/components/stop-info/stop-info.jsx
+++ src/components/stop-info/stop-info.jsx
@@ -1,17 +1,22 @@
 import { Component } from "react";
 import LineItem from "../line-item/line-item";
 
-
 export default class StopInfo extends Component {
-    render() {
-        return (
-            <>
-              {this.props.stopData.forEach((arrive) => {
-                console.log(arrive);
-                <LineItem data={arrive} />;
-              })}
-            </>
-          )
-    }
+  render() {
+    return (
+      <>
+        {this.props.stopData !== null
+          ? this.props.stopData.forEach((arrive) => {
+            console.log(arrive)
+              console.log(
+                `${parseFloat(arrive.estimateArrive / 60).toFixed(
+                  0
+                )} minutes to arrive!`
+              );
+              <LineItem data={arrive} />;
+            })
+          : ""}
+      </>
+    );
+  }
 }
-
