b0398750c8ba884526c96658170ae4c0e42bbe9b
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-01 12:54:49 +0200

    NEW api calls

---
+++ src/utils/apiCalls.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+export const login = async () => {
+    const {data: response} = await axios.get(
+        process.env.EMT_BASE_URL ?? 'https://openapi.emtmadrid.es/v1'  + `/mobilitylabs/user/login/`,
+        {
+            headers: {
+                email: process.env.USER_EMAIL ?? 'alejandro.gallardo.cuentas@gmail.com',
+                password: process.env.USER_PASS ?? 'Ro123456.'
+            }
+        })
+    return await response.data[0]
+}
+
+export const getTimeArrival = async (stopNumber, accessToken) => {
+    const {data: response} = await axios.post(
+        process.env.EMT_BASE_URL ?? 'https://openapi.emtmadrid.es/v2'  + `/transport/busemtmad/stops/${stopNumber}/arrives/`,
+        {
+            cultureInfo: 'ES',
+            Text_StopRequired_YN: 'Y',
+            Text_EstimationsRequired_YN: 'Y',
+            Text_IncidencesRequired_YN: 'N',
+            DateTime_Referenced_Incidencies_YYYYMMDD: '????????'
+        },
+        {
+            headers: {
+               accessToken,
+               'Access-Control-Allow-Origin': '*',
+               'Content-Type': 'application/json'
+            },
+        })
+    return response.data[0]
+}
