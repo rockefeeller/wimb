4ff380c5548698b67ef129566268ae9859268ebf
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-02 21:03:40 +0200

    UPDATE getTimeArrival response

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -29,5 +29,5 @@ export const getTimeArrival = async (stopNumber, accessToken) => {
                'Content-Type': 'application/json'
             },
         })
-    return response.data[0]
+    return response.data
 }
