9947639398ca1e0ee019c59398bfe0c1190a5cf6
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-10 21:39:31 +0200

    UPDATE user browser router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Container } from "@mui/material";
+import MenuComponent from "./components/menu/menu";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+    <MenuComponent />
+    <br></br>
+      <Container fixed>
+        <App />
+      </Container>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
