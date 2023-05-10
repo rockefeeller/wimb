3012220d91ef76786e6a8678ea9aa2a038675907
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-10 21:39:10 +0200

    UPDATE delete contanier for grid

--- src/components/line-list/line-list.jsx
+++ src/components/line-list/line-list.jsx
@@ -3,7 +3,7 @@ import ArriveItem from "../arrive-item/arrive-item";
 
 const LineList = ({ arrives }) => {
   return (
-      <Grid container spacing={1}>
+      <Grid spacing={1}>
         {arrives.map((arrive, index) => 
           <Grid item xs={10}>
             <ArriveItem arrive={arrive} />
