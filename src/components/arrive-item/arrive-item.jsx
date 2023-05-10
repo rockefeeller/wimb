cf570b74aab4e928e446b0488054c0a45519158c
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-10 21:37:29 +0200

    UPDATE apply styles and laod images

--- src/components/arrive-item/arrive-item.jsx
+++ src/components/arrive-item/arrive-item.jsx
@@ -1,24 +1,35 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, Typography, duration, keyframes, styled } from "@mui/material";
 import AddLocationIcon from "@mui/icons-material/AddLocation";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import DirectionsBusIcon from "@mui/icons-material/DirectionsBus";
 const ArriveItem = ({ arrive }) => {
+
+  const imgStyles = {
+      width:'100px', 
+      transform: 'scaleX(-1)',
+      float: 'left', 
+      "@keyframes move": {
+        transform: 'translateX(1)'
+      }
+    }
+
   return (
     <Card variant="outlined">
       <CardContent>
         <Typography>
-          <DirectionsBusIcon />
-          {arrive.line}
+          <DirectionsBusIcon />{arrive.line}
         </Typography>
+        <img src={require('../../media_resources/bus.gif')} style={imgStyles} alt="" />
         <Typography>
           <AccessTimeIcon />
           {parseFloat(arrive.estimateArrive / 60).toFixed(0)} min
         </Typography>
-        <Typography>
+        {/* <p style={{float: 'rigth'}}>
           <AddLocationIcon />
           {arrive.destination}
-        </Typography>
+        </p> */}
       </CardContent>
+      
     </Card>
   );
 };
