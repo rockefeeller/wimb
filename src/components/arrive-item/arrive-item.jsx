96b891042e397e0f6924443ac071bb8fd0c2c435
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-11 11:30:27 +0200

    UPDATE first animation

--- src/components/arrive-item/arrive-item.jsx
+++ src/components/arrive-item/arrive-item.jsx
@@ -4,14 +4,25 @@ import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import DirectionsBusIcon from "@mui/icons-material/DirectionsBus";
 const ArriveItem = ({ arrive }) => {
 
-  const imgStyles = {
-      width:'100px', 
-      transform: 'scaleX(-1)',
-      float: 'left', 
-      "@keyframes move": {
-        transform: 'translateX(1)'
-      }
+  const calculateMove = (time) => {
+    if(time > 10) return 0;
+    return (10-time) * 100
+    /**
+     * 10 - time * 100
+     */
+  }
+
+  const move = keyframes`
+    to{
+      transform: translateX(${calculateMove(parseFloat(arrive.estimateArrive / 60).toFixed(0))+"px"}) scaleX(-1) 
     }
+  `
+
+  const BusMove = styled("img")({
+    float: 'left',
+    width:'100px',
+    animation: `${move} 5s`,
+  })
 
   return (
     <Card variant="outlined">
@@ -19,7 +30,7 @@ const ArriveItem = ({ arrive }) => {
         <Typography>
           <DirectionsBusIcon />{arrive.line}
         </Typography>
-        <img src={require('../../media_resources/bus.gif')} style={imgStyles} alt="" />
+        <BusMove src={require('../../media_resources/bus-unscreen.gif')} style={{transform: 'scaleX(-1)'}} alt="" />
         <Typography>
           <AccessTimeIcon />
           {parseFloat(arrive.estimateArrive / 60).toFixed(0)} min
