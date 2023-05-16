7169d6fc785c779939d8121734b5f06774ce8e48
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-16 20:05:57 +0200

    UPDATE use styles

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,10 +1,28 @@
+import {keyframes, styled } from "@mui/material";
+
 const Home = () => {
+
+  const move = keyframes`
+    from {
+      translateX(0px)
+    }
+    to{
+      transform: translateX(900px) scaleX(-1) 
+    }
+  `
+
+  const BusMove = styled("img")({
+    animation: `${move} 10s forwards`,
+  })
+
   return (
-    <div>
-      <h1>Welcome to Wimb!</h1>
+    <>
+    <div style={{alignContent: 'center'}}>
+      <img src={require('../../media_resources/wimb_logo.png')} alt="" />
       <h4>The site where you can know where is your bus!</h4>
-      <img src={require('../../media_resources/bus.gif')} style={{transform: 'scaleX(-1)'}}alt=""/>
     </div>
+    <BusMove src={require('../../media_resources/bus.gif')} style={{transform: 'scaleX(-1)', width:'200px'}}alt=""/>
+    </>
   );
 };
 
