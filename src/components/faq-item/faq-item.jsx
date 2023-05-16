3d1f283dddb42c245f15c187b7bb5c50f891674c
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-16 20:03:43 +0200

    NEW FAQS component

---
+++ src/components/faq-item/faq-item.jsx
@@ -0,0 +1,33 @@
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const FaqItem = ({
+    title,
+    description
+}) => {
+    return (
+<Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <ul>
+            {
+                description.map(
+                    (item) => <li>{item}</li>  
+                )
+            }
+            </ul>
+        </AccordionDetails>
+      </Accordion>
+    )
+}
+
+export default FaqItem
---
+++ src/components/faqs/faqs.jsx
@@ -0,0 +1,31 @@
+import ArriveItem from "../arrive-item/arrive-item";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import FaqItem from "../faq-item/faq-item";
+
+const Faqs = () => {
+  return (
+    <>
+    <br />
+    <FaqItem
+      title={"How can i search a bus info?"}
+      description={[
+        "Go to \"Where is my bus\" section on the menu.",
+        "Introduce in the input the stop number which do you want to search."
+      ]}
+    />
+    <FaqItem 
+      title={"Contact with us"}
+      description={[
+        "Send an email to the following email address.",
+        "alejandro.gallardo@gmail.com"
+      ]}
+    />
+    </>
+  );
+};
+
+export default Faqs;
