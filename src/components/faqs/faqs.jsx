4fa39c6336f8e1e144c38e2692eb6d96339fe041
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-25 19:20:40 +0200

    UPDATE new faqs

--- src/components/faqs/faqs.jsx
+++ src/components/faqs/faqs.jsx
@@ -10,25 +10,32 @@ const Faqs = () => {
         text={
           "Para buscar la información sobre la parada que deseas has de seguir los siguientes pasos:"
         }
-        description={[
-          'Dirigete a la sección "Where is my bus" del menú.',
+        items={[
+          'Dirigete a la sección "¿Dónde está mi bus?" del menú.',
           "En el campo de texto introduce el número de parada que deseas buscar.",
           'Pulsa en el botón "Buscar".',
         ]}
+      />
+      <FaqItem
+        title={"¿De dónde extráeis la información?"}
+        text={
+          "Toda la información es extraída de la API publicada por la empresa municipal de transportes EMT.\n Podéis encontrar toda la documentación en el siguiente enlace: https://mobilitylabs.emtmadrid.es/"
+        }
+        items={[]}
       />
       <FaqItem
         title={"¿Tienes alguna discapacidad?"}
         text={
           'Si padeces alguna discapacidad motriz puedes indicarlo en la sección del menu llamada "Accesibilidad", ésto te permitirá realizar las búsquedas utilizando el micrófono de tu dispositivo, ya se móvil, tablet u ordenador.'
         }
-        description={[]}
+        items={[]}
       />
       <FaqItem
-        title={"¿Cómo contacto con ustedes?"}
+        title={"¿Cómo contactar con nosotros?"}
         text={
-          'Para poder contactar con el equipo de soporte, dirigete a menu->Contacto, encontrarás un botón con el texto "Contactar", al pulsarlo se abrirá tu cliente de correo con la dirección de soporte precargada.'
+          'Para poder contactar con el equipo de soporte, dirigete a menu => Contacto, encontrarás un botón con el texto "Contactar", al pulsarlo se abrirá tu cliente de correo con la dirección de soporte precargada.'
         }
-        description={[]}
+        items={[]}
       />
     </>
   );
