47b9424bbd64bdda79edd09d6c400569760a5c8d
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-25 19:21:07 +0200

    UPDATE show alert when stop doesn't exist

--- src/components/stop-info/stop-info.jsx
+++ src/components/stop-info/stop-info.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import ArriveList from "../arrive-list/arrive-list";
+import { Alert } from "@mui/material";
 
-const StopInfo = ({ stopData }) => {
-  return stopData.map((stop) => <ArriveList arrives={stop["Arrive"]} />);
+const StopInfo = ({ stopData, stopNumber }) => {
+  return stopData[0]["Arrive"].length === 0 ? (
+    <Alert severity="warning">
+      No existe la parada {stopNumber}.
+    </Alert>
+  ) : (
+    stopData.map((stop) => <ArriveList arrives={stop["Arrive"]} />)
+  );
 };
 
 export default StopInfo;
