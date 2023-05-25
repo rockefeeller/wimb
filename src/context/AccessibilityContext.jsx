bcb8e8236297c8776d9a43a5ea4d1ac6532cd981
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-05-25 19:42:26 +0200

    UPDATE load context state

--- src/context/AccessibilityContext.jsx
+++ src/context/AccessibilityContext.jsx
@@ -3,8 +3,9 @@ import React, { useState } from "react";
 export const AccessibilityContext = React.createContext({});
 
 export default function AccessibilityContextProvider({ children }) {
-  const [userHasHandicap, setUserHasHandicap] = useState(Boolean(localStorage.getItem('userHasHandicap')) ?? false);
-  const [userHasVisualHandicap, setUserHasVisualHandicap] = useState(Boolean(localStorage.getItem('userHasVisualHandicap')) ?? false);
+  useState(localStorage.getItem('userHasHandicap') === 'true' ? true : false)
+  const [userHasHandicap, setUserHasHandicap] = useState(localStorage.getItem('userHasHandicap') === 'true' ? true : false)
+  const [userHasVisualHandicap, setUserHasVisualHandicap] = useState(localStorage.getItem('userHasVisualHandicap') === 'true' ? true : false)
   const [titleColor, setTitleColor] = useState(localStorage.getItem('titleColor') ?? '#577eeb')
 
   return (
