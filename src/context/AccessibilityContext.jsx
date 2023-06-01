d5c2cdedfe7510e3510a96f128daa6b1f63915bd
rockefeeller <alejandro.gallardo.cuentas@gmail.com>
2023-06-01 18:33:03 +0200

    REFACTOR context

--- src/context/AccessibilityContext.jsx
+++ src/context/AccessibilityContext.jsx
@@ -3,10 +3,16 @@ import React, { useState } from "react";
 export const AccessibilityContext = React.createContext({});
 
 export default function AccessibilityContextProvider({ children }) {
-  useState(localStorage.getItem('userHasHandicap') === 'true' ? true : false)
-  const [userHasHandicap, setUserHasHandicap] = useState(localStorage.getItem('userHasHandicap') === 'true' ? true : false)
-  const [userHasVisualHandicap, setUserHasVisualHandicap] = useState(localStorage.getItem('userHasVisualHandicap') === 'true' ? true : false)
-  const [titleColor, setTitleColor] = useState(localStorage.getItem('titleColor') ?? '#577eeb')
+  useState(localStorage.getItem("userHasHandicap") === "true" ? true : false);
+  const [userHasHandicap, setUserHasHandicap] = useState(
+    localStorage.getItem("userHasHandicap") === "true" ? true : false
+  );
+  const [userHasVisualHandicap, setUserHasVisualHandicap] = useState(
+    localStorage.getItem("userHasVisualHandicap") === "true" ? true : false
+  );
+  const [titleColor, setTitleColor] = useState(
+    localStorage.getItem("titleColor") ?? "#577eeb"
+  );
 
   return (
     <AccessibilityContext.Provider
@@ -16,7 +22,7 @@ export default function AccessibilityContextProvider({ children }) {
         userHasVisualHandicap,
         setUserHasVisualHandicap,
         titleColor,
-        setTitleColor
+        setTitleColor,
       }}
     >
       {children}
